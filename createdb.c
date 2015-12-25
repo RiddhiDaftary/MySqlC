9c8e35f90a3fb5e98aeefeab0fc3be34453a09f1
Harshit Daftary <daftaryharshit@yahoo.co.in>
2015-12-26 02:33:59 +0530

    added comand line support

--- createdb.c
+++ createdb.c
@@ -2,7 +2,7 @@
 #include <mysql.h>
 #include <string.h>
 
-int main(int argc, char **argv) // main function
+int main(int argc, char *argv[]) // main function
 {
     char strDBName[100];
     MYSQL *con = mysql_init(NULL);
@@ -21,10 +21,21 @@ int main(int argc, char **argv) // main function
         exit(0);
     }
     
-    printf("\nEnter the name of a database : \n");
+printf("arguments - %d",argc);
+printf("first arg - %s",argv[0]);
+
+if (argc != 1)
+{
+    printf("\nEnter the name of the database : \n");
     scanf("%s",strDBName);
+}
+else
+{
+//	strDBName = &argv[0];
+strcpy(strDBName,argv[0]);
+}
     strcat(strQuery,strDBName);
-   // printf("\n Query is -> : %s \n",strQuery); 
+    printf("\n Query is -> : %s \n",strQuery); 
     
     if (mysql_query(con, strQuery))
     {
